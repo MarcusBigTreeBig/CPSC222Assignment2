e49876dd943274e08f363e4f25cfe19a3fb34af7
MarcusBigTreeBig <the.marcus.herbert@gmail.com>
2024-02-09 00:13:02 -0800

    going to rewrite IntakeQueue

--- src/Hospital/IntakeQueue.java
+++ src/Hospital/IntakeQueue.java
@@ -5,9 +5,22 @@ import java.util.Comparator;
 public class IntakeQueue {
     private IntakeNode front;
     private IntakeNode rear;
+    private Comparator<Intake> comparator;
     public IntakeQueue () {
         front = null;
         rear = null;
+        comparator = new Comparator<Intake> () {
+            @Override
+            public int compare(Intake o1, Intake o2) {
+                if (o1.getType() == o2.getType()) {
+                    return o1.getSeverity() - o2.getSeverity();
+                }
+                if (o1.getType() == IntakeType.Emergency) {
+                    return 1;
+                }
+                return -1;
+            }
+        };
     }
 
     public synchronized Intake dequeue () {
@@ -16,52 +29,41 @@ public class IntakeQueue {
         }
         IntakeNode temp = front;
         front = front.back;
-        front.next = null;
+        if (front != null) {
+            front.next = null;
+        }
         return temp.intake;
     }
 
     public synchronized void addIntake (Intake in) {
-        IntakeNode current = rear;
         IntakeNode toAdd = new IntakeNode();
         toAdd.intake = in;
         if (rear == null) {
-            rear = toAdd;
             front = toAdd;
-        }else {
-            Comparator<IntakeType> comp = new Comparator<IntakeType>() {
-                @Override
-                public int compare(IntakeType o1, IntakeType o2) {
-                    if (o1 == o2) {
-                        return 0;
-                    }
-                    if (o1 == IntakeType.Appointment) {
-                        return -1;
-                    }
-                    if (o1 == IntakeType.General && o2 == IntakeType.Emergency) {
-                        return -1;
-                    }
-                    if (o1 == IntakeType.General) {
-                        return 1;
-                    }
-                    if (o2 == IntakeType.Emergency) {
-                        return -1;
-                    }
-                    return 1;
-                }
-            };
-            while (current != null && comp.compare(toAdd.intake.getType(), current.intake.getType()) < 0) {
-                current = current.next;
-            }
-            while (current != null && toAdd.intake.getSeverity() < current.intake.getSeverity()) {
+            rear = toAdd;
+        }else{
+            IntakeNode current = rear;
+            while (current.next != null && comparator.compare(toAdd.intake, current.intake) < 0) {
                 current = current.next;
             }
             toAdd.next = current;
-            if (current != null && current.back != null) {
-                toAdd.back = current.back.next;
+            toAdd.back = current.back;
+            if (current.back != null) {
                 current.back.next = toAdd;
-                current.back = toAdd;
             }
+            current.back = toAdd;
         }
+        traverse();
+    }
+
+    public synchronized void traverse () {
+        System.out.println("*** start of traverse ***");
+        IntakeNode current = rear;
+        while (current != front) {
+            System.out.println(current.intake);
+        }
+        System.out.println(current.intake);
+        System.out.println("*** end of traverse ***");
     }
 
     private class IntakeNode{
